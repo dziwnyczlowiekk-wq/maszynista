3185899ef2bdc148d73f46562f5c69d1c18a116d
dziwnyczlowiekk-wq <dziwnyczlowiekk@gmail.com>
2026-05-25 21:22:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
   Klucz "anon public" może być na stronie, ponieważ dostęp ograniczają polityki RLS.
 */
 window.WEDDING_CONFIG = {
-  SUPABASE_URL: "https://bzhhcxzykusutnnzdjmm.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://bzhhcxzykusutnnzdjmm.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6aGhjeHp5a3VzdXRubnpkam1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MjM5OTIsImV4cCI6MjA5NTI5OTk5Mn0.ZHEcPDJTfks_yTbOkgF4KErynS7A8sAJoCNc27St8KU",
 
   COUPLE_NAMES: "Agnieszka & Adam",
