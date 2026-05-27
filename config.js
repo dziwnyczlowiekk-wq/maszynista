826c4142b7bffb2665558541403e0328129f7b78
dziwnyczlowiekk-wq <dziwnyczlowiekk@gmail.com>
2026-05-27 08:56:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.WEDDING_CONFIG = {
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6aGhjeHp5a3VzdXRubnpkam1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MjM5OTIsImV4cCI6MjA5NTI5OTk5Mn0.ZHEcPDJTfks_yTbOkgF4KErynS7A8sAJoCNc27St8KU",
 
   COUPLE_NAMES: "Agnieszka & Adam",
-  WEDDING_DATE: "17 Lipca 2026",
+  WEDDING_DATE: "17 lipca 2026",
   HERO_IMAGE: "para.png",
   ALBUM_SUBTITLE: "Zdjęcia przesłane przez naszych najbliższych."
 };
